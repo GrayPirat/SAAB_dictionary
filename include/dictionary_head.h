4e4262e6ec76b8ea3d0e72133ee58831acbcd8ff
Aleksey <yuhayo@mail.ru>
2024-02-13 17:37:26 +0300

    head and inits

---
+++ include/dictionary_head.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include <string>
+#include <iosfwd>
+using namespace std;
+
+template<typename KeyData, typename Data>
+class Table
+{
+	protected:
+	typedef _Vector_iterator<_Vector_val<_Simple_types<pair<KeyData, Data>>>> it; //type of vector iterator 
+
+	vector<pair<KeyData, Data>> arr; // dictionary
+public:
+	it& insert(KeyData key, Data value); //insert cacheline 
+	bool Remove(KeyData key); // remove ...
+	it& operator[](KeyData key);// access to line`s data
+	it search(KeyData key);// search for line with it`s key
+};
--- samples/sample_dict.cpp
+++ samples/sample_dict.cpp
@@ -1,5 +1,72 @@
 #include "dictionary_head.h"
+using namespace std;
+template<typename KeyData,typename Data>
+
+bool Table<KeyData, Data>::Remove(KeyData key)
+{
+	auto iter = arr.begin();
+	
+	while (iter != arr.end())
+	{
+		if (iter->first == key)
+		{
+			arr.erase(iter);
+			return true;
+		}
+		++iter;
+	}
+	return false;
+}
+template<typename KeyData, typename Data>
+typename Table <KeyData,Data>:: it& Table<KeyData, Data>::insert(KeyData key, Data value)
+{
+	auto iter = arr.begin();
+	while (iter != arr.end())
+	{
+
+		if (iter->first == key)
+		{
+			iter->second = value;
+			return iter;
+		}
+		++iter;
+	}
+	arr.push_back(make_pair(key, value));
+	return iter;
+}
+template<typename KeyData, typename Data>
+typename Table <KeyData, Data>::it Table<KeyData, Data>::search(KeyData key)
+{
+	auto iter = arr.begin();
+	while (iter != arr.end())
+	{
+		if (key == iter->first)
+		{
+			return iter;
+		}
+		++iter;
+	}
+	return iter;
+}
+template<typename KeyData, typename Data>
+typename Table <KeyData, Data>::it& Table<KeyData, Data>::operator[](KeyData key)
+{
+	auto iter = arr.begin();
+	while (iter != arr.end())
+	{
+		if (iter->first == key)
+			cout << iter->second;
+		return iter;
+		iter++;
+	}
+	return iter;
+}
 
 int main() {
-	return 0;
+	Table<int,string> a;
+	a.insert(2, "tyty");
+	a.insert(3, "tyty");
+	a.insert(1, "ty");
+	a.Remove(1);
+	a[2];
 }

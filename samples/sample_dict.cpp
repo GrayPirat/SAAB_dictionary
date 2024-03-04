8487b5906f269fd5ccba7bffad10cef1b501a9c1
Aleksey <yuhayo@mail.ru>
2024-03-04 18:54:55 +0300

    pizda

--- samples/sample_dict.cpp
+++ samples/sample_dict.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int main(){
-	Hash_Table<string, int> test;
+	Hash_Table<string, int> test(2000);
 	test.insert_hash("haha", 1);
 	test.insert_hash("haha", 3);
 	test.insert_hash("haha", 5);
 	test.insert_hash("bad", 5);
 
-	/*test.remove_hash("haha");*/
-	/*test["bad"]->second;*/
+	/*test.remove_hash("haha");
+	test["bad"]->second;
+	*/
 	
-	/*Hash_Table<int, string> test2;
-	test2.insert_hash(234, "sgsdg");*/
 }

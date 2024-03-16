a0ea8e9303d38acff3c79b70849099275e64d224
Aleksey <yuhayo@mail.ru>
2024-03-16 12:59:44 +0300

    done iterator

--- samples/sample_dict.cpp
+++ samples/sample_dict.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 	{
-		Hash_Table<string, int> test(2000);
+		Hash_Table<string, int> test(20);
 		test.insert_hash("haha", 1);
 		test.insert_hash("haha", 3);
 		test.insert_hash("haha", 5);
 		test.insert_hash("bad", 5);
 		test.remove_hash("bad");
-		test.search_hash("bad");
 
 		test["bad"]->second = 5;
-		test.operator++();
+		test.operator++()->second=1;
+		test.operator++()->second=1;
 	}
 	
 	
